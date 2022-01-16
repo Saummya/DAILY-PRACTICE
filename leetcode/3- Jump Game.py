778df965f96c1cb859979b8c157271a20e2afe07
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-16 13:02:02 +0530

    https://leetcode.com/problems/jump-game/

--- leetcode/3- Jump Game.py
+++ leetcode/3- Jump Game.py
@@ -1,17 +1,16 @@
 class Solution:
-    def jump(self, nums: List[int]) -> int:
-        i=len(nums)-1
-        j=0
-        count=0
-        while i > 0 :
-            while j < i:
-                if i <= j+nums[j]:
-                    #print(i,j)
-                    i=j
-                    j=0
-                    count+=1
-                    break
-                else:
-                    j+=1
-            #return False
-        return(count)
+    def canJump(self, nums: List[int]) -> bool:
+            '''
+            goal=len(nums)-1
+            for i in range(len(nums)-1,-1,-1):
+                if i+nums[i]>i:
+                    goal=i
+
+            return True if goal==0 else False
+            '''
+            goal = 0
+            for i in range(len(nums)):
+                if i > goal:
+                    return False
+                goal = max(goal, nums[i] + i)
+            return True
