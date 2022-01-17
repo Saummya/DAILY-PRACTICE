758d90ce364853f963dffaafe21f95a370e50d53
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-17 19:56:20 +0530

    https://practice.geeksforgeeks.org/problems/ishaan-loves-chocolates2156/1/?category[]=Greedy&category[]=Greedy&difficulty[]=-1&page=1&query=category[]Greedydifficulty[]-1page1category[]Greedy

---
+++ gfg/4-Ishaan Loves Chocolates.py
@@ -0,0 +1,14 @@
+
+#WE JUST NEED TO FIND THE CHOCOLATE BLOCK WITH MIN TASTINESS
+
+def chocolates (arr, n) : 
+    #Complete the function
+    
+    min=arr[0]
+    for i in range(n):
+        if min<arr[i]:
+            continue
+        else:
+            min=arr[i]
+    return min
+        
