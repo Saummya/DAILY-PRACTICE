0a801d83b898e2b592b57b73130c396a0199b0ba
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-19 02:06:57 +0530

    https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1/?category[]=Greedy&category[]=Greedy&difficulty[]=0&page=1&query=category[]Greedydifficulty[]0page1category[]Greedy#

--- leetcode/6-N meetings in one room.py
+++ leetcode/6-N meetings in one room.py
@@ -1,10 +1,12 @@
+#User function Template for python3
+
 class Solution:
     
     #Function to find the maximum number of meetings that can
     #be performed in a meeting room.
     def maximumMeetings(self,n,start,end):
         # code here
-        
+        #compare end[i] with start[i+1] if start[i+1] is greater than end[i] then only meeting will happen.
         a=[]
         for i in range(len(start)):
             a.append([start[i],end[i]])
@@ -22,3 +24,25 @@ class Solution:
                 j+=1
         return count
         
+
+                
+                
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        start = list(map(int,input().strip().split()))
+        end = list(map(int,input().strip().split()))
+        ob=Solution()
+        print(ob.maximumMeetings(n,start,end))
+# } Driver Code Ends
