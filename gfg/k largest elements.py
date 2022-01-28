9421d29cbc06c22fe5db31f3d874e3a2906ff32e
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-29 00:55:35 +0530

    https://practice.geeksforgeeks.org/problems/k-largest-elements4206/1/?category[]=Arrays&category[]=Arrays&difficulty[]=1&page=1&query=category[]Arraysdifficulty[]1page1category[]Arrays#

---
+++ gfg/k largest elements.py
@@ -0,0 +1,61 @@
+
+'''
+Medium, Accuracy: 53.0% Submissions: 33426 Points: 4
+Given an array Arr of N positive integers, find K largest elements from the array.  The output elements should be printed in decreasing order.
+
+Example 1:
+
+Input:
+N = 5, K = 2
+Arr[] = {12, 5, 787, 1, 23}
+Output: 787 23
+Explanation: 1st largest element in the
+array is 787 and second largest is 23.
+Example 2:
+
+Input:
+N = 7, K = 3
+Arr[] = {1, 23, 12, 9, 30, 2, 50}
+Output: 50 30 23
+Explanation: 3 Largest element in the
+array are 50, 30 and 23.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function kLargest() which takes the array of integers arr, n and k as parameters and returns an array of integers denoting the answer. The array should be in decreasing order.
+
+Expected Time Complexity: O(N + KlogK)
+Expected Auxiliary Space: O(K+(N-K)*logK)
+
+Constraints:
+1 ≤ K ≤ N ≤ 105
+1 ≤ Arr[i] ≤ 106
+'''
+
+#User function Template for python3
+class Solution:
+
+	def kLargest(self,arr, n, k):
+		# code here
+		arr.sort()
+		l=[]
+		for i in range(n-1,n-k-1,-1):
+		    l.append(arr[i])
+		return l
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, k = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.kLargest(arr, n, k)
+        for x in ans:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
