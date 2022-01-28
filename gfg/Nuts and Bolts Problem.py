dbf4b79666dbb971515cd249e5394fdee5972948
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-29 01:02:27 +0530

    https://practice.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1/?category[]=Arrays&category[]=Arrays&difficulty[]=1&page=1&query=category[]Arraysdifficulty[]1page1category[]Arrays#

---
+++ gfg/Nuts and Bolts Problem.py
@@ -0,0 +1,57 @@
+'''
+Given a set of N nuts of different sizes and N bolts of different sizes. There is a one-one mapping between nuts and bolts. Match nuts and bolts efficiently.
+
+Comparison of a nut to another nut or a bolt to another bolt is not allowed. It means nut can only be compared with bolt and bolt can only be compared with nut to see which one is bigger/smaller.
+The elements should follow the following order ! # $ % & * @ ^ ~ .
+
+Example 1:
+
+Input: 
+N = 5
+nuts[] = {@, %, $, #, ^}
+bolts[] = {%, @, #, $ ^}
+Output: 
+# $ % @ ^
+# $ % @ ^
+Example 2:
+
+Input: 
+N = 9
+nuts[] = {^, &, %, @, #, *, $, ~, !}
+bolts[] = {~, #, @, %, &, *, $ ,^, !}
+Output: 
+! # $ % & * @ ^ ~
+! # $ % & * @ ^ ~
+'''
+#User function Template for python3
+class Solution:
+
+	def matchPairs(self,nuts, bolts, n):
+		# code here
+		nuts.sort()
+		bolts.sort()
+		
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        nuts = list(map(str, input().strip().split()))
+        bolts = list(map(str, input().strip().split()))
+        ob = Solution()
+        ob.matchPairs(nuts, bolts, n)
+        for x in nuts:
+            print(x, end=" ")
+        print()
+        for x in bolts:
+            print(x, end=" ")
+        print()
+        tc -= 1
+
+# } Driver Code Ends
