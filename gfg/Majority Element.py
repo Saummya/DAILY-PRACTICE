f5da1885a80328436ab42e04dee4a9184ebeeb35
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-01-30 04:31:37 +0530

    https://practice.geeksforgeeks.org/problems/majority-element-1587115620/1/?difficulty[]=1&page=1&category[]=Arrays&query=difficulty[]1page1category[]Arrays#

---
+++ gfg/Majority Element.py
@@ -0,0 +1,89 @@
+'''
+Medium Accuracy: 48.6% Submissions: 100k+ Points: 4
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+Example 2:
+
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+Your Task:
+The task is to complete the function majorityElement() which returns the majority element in the array. If no majority exists, return -1.
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+ 
+
+Constraints:
+1 ≤ N ≤ 107
+0 ≤ Ai ≤ 106
+'''
+#User function template for Python 3
+
+class Solution:
+    def majorityElement(self, A, N):
+        #Your code here
+        max1=A[0]
+        for i in range(1,N):
+            if A[i]>max1:
+                max1=A[i]
+                
+        a=[0]*(max1+1)
+        for i in range(N):
+            a[A[i]]+=1
+            
+        for i in range(max1+1):
+            if a[i]>N//2:
+                return i
+        return -1   
+            
+            
+        
+        
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+from sys import stdin
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+
+            A=[int(x) for x in input().strip().split()]
+            
+            
+            obj = Solution()
+            print(obj.majorityElement(A,N))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
