e6824ed69b7d31b3acd999231316cd407f556eb9
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-02 02:15:44 +0530

    easy

---
+++ gfg/Segregate 0s and 1s.py
@@ -0,0 +1,54 @@
+'''
+Given an array of length N consisting of only 0s and 1s in random order. Modify the array to segregate 0s on left side and 1s on the right side of the array.
+
+Example 1:
+
+Input:
+N = 5
+arr[] = {0, 0, 1, 1, 0}
+Output: 0 0 0 1 1
+
+Example 2:
+
+Input:
+N = 4
+Arr[] = {1, 1, 1, 1}
+Output: 1 1 1 1
+Explanation: There are no 0 in the given array, 
+so the modified array is 1 1 1 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function segregate0and1() which takes arr[] and n as input parameters and modifies arr[] in-place without using any extra memory.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 107
+0 ≤ arri ≤ 1
+'''
+#User function Template for python3
+
+class Solution:
+    def segregate0and1(self, arr, n):
+        # code here
+        arr.sort()
+        return arr
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ob.segregate0and1(arr, n)
+        print(*arr)
+        tc -= 1
+
+# } Driver Code Ends
