e858fc6a991e935c42221ddc9e6f94471587d57e
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-04 03:21:31 +0530

    medium

---
+++ gfg/AMAZON ASKED/Print Anagrams Together.py
@@ -0,0 +1,91 @@
+'''
+Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array. Look at the sample case for clarification.
+
+
+Example 1:
+
+Input:
+N = 5
+words[] = {act,god,cat,dog,tac}
+Output:
+act cat tac 
+god dog
+Explanation:
+There are 2 groups of
+anagrams "god", "dog" make group 1.
+"act", "cat", "tac" make group 2.
+Example 2:
+
+Input:
+N = 3
+words[] = {no,on,is}
+Output: 
+no on
+is
+Explanation:
+There are 2 groups of
+anagrams "no", "on" make group 1.
+"is" makes group 2. 
+
+Your Task:
+The task is to complete the function Anagrams() that takes a list of strings as input and returns a list of groups such that each group consists of all the strings that are anagrams.
+
+
+Expected Time Complexity: O(N*|S|*log|S|), where |S| is the length of the strings.
+Expected Auxiliary Space: O(N*|S|), where |S| is the length of the strings.
+
+
+Constraints:
+1<=N<=100
+
+1<=|S|<=10
+'''
+#User function Template for python3
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        
+        #code here
+        #idea: sort all the words and for each sortedWord, add the word in a dictionary 
+        #for that sortedWord. In the end, you'll get 
+        #a dictionary with groups of anagrams
+        dict = {}
+        lst = []
+        for word in words:
+           #sort the word
+            newWord = ''.join(sorted(word))
+            if newWord in dict.keys():
+                dict[newWord].append(word)
+            else:
+                dict[newWord]=[]
+                dict[newWord].append(word)
+        for key in dict.keys():
+            lst.append(dict[key])
+       
+        return lst
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
