f090af3ed09594849a5b6873d2b18be94da126e4
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-04 02:12:00 +0530

    basic

---
+++ gfg/AMAZON ASKED/Maximum money.py
@@ -0,0 +1,52 @@
+'''
+Given a street of N houses (a row of houses), each house having K amount of money kept inside; now there is a thief who is going to steal this money but he has a constraint/rule that he cannot steal/rob two adjacent houses. Find the maximum money he can rob.
+
+Example 1:
+
+Input:
+N = 5 , K = 10
+Output:
+30
+Explanation:
+The Robber can rob from the first, third
+and fifth houses which will result in 30.
+Example 2:
+
+Input:
+N = 2 , K = 12
+Output:
+12
+Explanation:
+The Robber can only rob from the first
+or second which will result in 12.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function maximizeMoney() which takes 2 Integers N and K as input and returns the answer.
+
+Expected Time Complexity: O(1)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N,K <= 103
+'''
+#User function Template for python3
+
+class Solution:
+    def maximizeMoney(self, N , K):
+        # code here 
+        c=0
+        for i in range(1,N+1,2):
+            c+=1
+        return c*K
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N,K = map(int,input().split())
+        
+        ob = Solution()
+        print(ob.maximizeMoney(N,K))
+# } Driver Code Ends
