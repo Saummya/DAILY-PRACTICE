e33b67e1bae73a8f1128b34ab7018535a7e0b053
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-23 21:59:54 +0530

    medium

---
+++ gfg/STRINGS/Multiply two strings.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:
+    def multiplyStrings(self,s1,s2):
+        # code here
+        # return the product string
+        m=int(s1)*int(s2)
+        return m
