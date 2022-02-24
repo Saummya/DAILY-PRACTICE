7246b679d63488ba1af3d80f6350f587ecf2228c
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-02-25 00:40:42 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.py
@@ -0,0 +1,62 @@
+class Solution:
+    #User function Template for python3
+    
+    #Complete this function
+    def findFloor(self,A,N,X):
+        #Your code here
+        l=0
+        h=N-1
+        res=-1
+        while(l<=h):
+            mid=(l+h)//2
+            if A[mid]==X:
+                return mid
+            elif A[mid]>X:
+                h=mid-1
+            else:
+                res=mid
+                l=mid+1
+        return res
+            
+        
+        
+        '''
+        l1=[]
+        for i in range(N):
+            if A[i]<=X:
+                l1.append(A[i])
+        if l1==[]:
+            return -1
+        else:
+            l1.sort()
+            return l1[n-1]'''
+                
+        
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            NX=[int(x) for x in input().strip().split()]
+            N=NX[0]
+            X=NX[1]
+
+            A=[int(x) for x in input().strip().split()]
+            
+            obj = Solution()
+            print(obj.findFloor(A,N,X))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
