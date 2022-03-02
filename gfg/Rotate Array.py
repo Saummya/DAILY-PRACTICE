de7cbba034745ebe88aa96d2db161deb0efb7f71
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-03-03 00:08:12 +0530

    rotatearray

---
+++ gfg/Rotate Array.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    #Function to rotate an array by d elements in counter-clockwise direction. 
+    def rotateArr(self,A,D,N):
+        #Your code here
+        temp=[0]*D
+        temp=A[:D]
+        for i in range(N-D):
+            A[i]=A[i+D]
+        A[N-D:]=temp
+     
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+def main():
+    T=int(input())
+    
+    while(T>0):
+        nd=[int(x) for x in input().strip().split()]
+        N=nd[0]
+        D=nd[1]
+        A=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.rotateArr(A,D,N)
+        
+        for i in A:
+            print(i,end=" ")
+            
+        print()
+       
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
