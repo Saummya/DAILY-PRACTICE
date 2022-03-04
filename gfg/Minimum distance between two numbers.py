4dab3345b3d883fa8ff558870df65d177f34513f
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-03-04 23:42:29 +0530

    min dist

---
+++ gfg/Minimum distance between two numbers.py
@@ -0,0 +1,65 @@
+'''
+You are given an array A, of N elements. Find minimum index based distance between two elements of the array, x and y.
+
+Example 1:
+
+Input:
+N = 4
+A[] = {1,2,3,2}
+x = 1, y = 2
+Output: 1
+Explanation: x = 1 and y = 2. There are
+two distances between x and y, which are
+1 and 3 out of which the least is 1.
+Example 2:
+
+Input:
+N = 7
+A[] = {86,39,90,67,84,66,62}
+x = 42, y = 12
+Output: -1
+Explanation: x = 42 and y = 12. We return
+-1 as x and y don't exist in the array.
+Your Task:
+Complete the function minDist() which takes the array, n, x and y as input parameters and returns the minimum distance between x and y in the array. If no such distance is possible then return -1.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 105
+0 <= A[i], x, y <= 105
+'''
+
+
+class Solution:
+    def minDist(self, arr, n, x, y):
+        diff=1000000
+        if x in arr and y in arr:
+            z={x:arr.index(x),y:arr.index(y)}
+            for i in range(n):
+                if arr[i]==x or arr[i]==y:
+                    z[arr[i]]=i
+                    if diff>abs(z[x]-z[y]):
+                        diff=abs(z[x]-z[y])
+            return diff
+        else:
+            return -1
+    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        x,y = list(map(int, input().strip().split()))
+        print(Solution().minDist(arr, n, x, y))
+
+
+
+# } Driver Code Ends
