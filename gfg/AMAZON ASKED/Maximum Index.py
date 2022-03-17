eae06f8952b694c5d7823f82ad2ccfc3537e953d
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-03-17 23:37:20 +0530

    medium

---
+++ gfg/AMAZON ASKED/Maximum Index.py
@@ -0,0 +1,87 @@
+'''
+Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] < A[j] and i < j.
+ 
+
+Example 1:
+
+Input:
+N = 2
+A[] = {1, 10}
+Output:
+1
+Explanation:
+A[0]<A[1] so (j-i) is 1-0 = 1.
+Example 2:
+
+Input:
+N = 9
+A[] = {34, 8, 10, 3, 2, 80, 30, 33, 1}
+Output:
+6
+Explanation:
+In the given array A[1] < A[7]
+satisfying the required 
+condition(A[i] < A[j]) thus giving 
+the maximum difference of j - i 
+which is 6(7-1).
+ 
+
+Your Task:
+The task is to complete the function maxIndexDiff() which finds and returns maximum index difference. Printing the output will be handled by driver code. Return -1 in case no such index is found.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 107
+0 ≤ A[i] ≤ 109
+'''
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    # Function to find the maximum index difference.
+    def maxIndexDiff(self,A, N): 
+        ##Your code here
+        i = 0
+        j = N-1
+        max1 = 0
+        
+        while i<N and j>i:
+            if A[i]<=A[j] and j>i:
+                max1 = max(max1,j-i)
+                i+=1
+                j=N-1
+            else:
+                j-=1
+            
+            if i>=j:
+                i+=1
+                j=N-1
+        
+        return max1
+        
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            ob=Solution()
+            print(ob.maxIndexDiff(arr,n))
+            
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
