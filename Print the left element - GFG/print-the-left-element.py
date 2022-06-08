260f6e4ffaec10061d3f0363d8d7be1ee2301941
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-08 10:27:03 +0530

    Added solution - LeetHub

---
+++ Print the left element - GFG/print-the-left-element.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def leftElement(self, arr, n):
+    # Your code goes here  
+        arr.sort()
+        if n==1:
+            return arr[0]
+        if n%2!=0:
+            return arr[(n-1)//2]
+        else:
+            return arr[(n-1)//2]
+            
+    
+    
+    
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        n = int(input())
+        a = [int(x) for x in input().strip().split()]
+        ob=Solution()
+        print(ob.leftElement(a, n))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+    
+# } Driver Code Ends
