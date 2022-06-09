bb6ad241dacee13abe6435b22be82a0bd0c3c846
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-10 01:20:13 +0530

    Added solution - LeetHub

--- Kadane's Algorithm - GFG/kadanes-algorithm.py
+++ Kadane's Algorithm - GFG/kadanes-algorithm.py
@@ -3,19 +3,33 @@
 class Solution:
     ##Complete this function
     #Function to find the sum of contiguous subarray with maximum sum.
-   
     def maxSubArraySum(self,arr,N):
         ##Your code here
-        max_so_far= arr[0]
-        max1=0
-        for i in range(N):
-            max1+=arr[i]
-            max_so_far=max(max_so_far,max1)
-            if max1<0:
-                max1=0
+        curr_sum=0
+        max_so_far=arr[0]
+        
+        st=0
+        en=0
+        poi=0
+        
+        for i in range(0,N):
+            curr_sum+=arr[i]
             
-        return max_so_far
+            if max_so_far<curr_sum:
+                max_so_far=curr_sum
+                st=poi
+                en=i
+                
+            if curr_sum<0:
+                curr_sum=0
+                poi=i+1
+                
+        s=0
+        for i in range(st,en+1):
+            s=s+arr[i]
             
+        return s
+                
 
 #{ 
 #  Driver Code Starts
