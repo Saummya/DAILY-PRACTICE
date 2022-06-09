90c04c68db2daa32329bbd26006f94effe2b0e7f
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-10 00:14:42 +0530

    Create Sort an array of 0s, 1s and 2s.py

---
+++ LOVE BABBAR DSA QUES/ARRAY/Sort an array of 0s, 1s and 2s.py
@@ -0,0 +1,71 @@
+'''
+Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+
+Example 1:
+
+Input: 
+N = 5
+arr[]= {0 2 1 2 0}
+Output:
+0 0 1 2 2
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+Example 2:
+
+Input: 
+N = 3
+arr[] = {0 1 0}
+Output:
+0 0 1
+Explanation:
+0s 1s and 2s are segregated 
+into ascending order.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function sort012() that takes an array arr and N as input parameters and sorts the array in-place.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+'''
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        # code here
+        zero=arr.count(0)
+        one=arr.count(1)
+        two=arr.count(2)
+        
+        for i in range(n):
+            if zero!=0:
+                arr[i]=0
+                zero-=1
+            elif one!=0:
+                arr[i]=1
+                one-=1
+            else:
+                arr[i]=2
+            
+        return arr
+      
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        n=int(input())
+        arr=[int(x) for x in input().strip().split()]
+        ob=Solution()
+        ob.sort012(arr,n)
+        for i in arr:
+            print(i, end=' ')
+        print()
+
+# } Driver Code Ends
