1f1ba275aa068545c9cf0bc9d8bf2a50e1ae6c7f
Saummya Singh <46069982+Saummya@users.noreply.github.com>
2022-06-09 23:58:10 +0530

    Added solution - LeetHub

---
+++ Print first letter of every word in the string - GFG/print-first-letter-of-every-word-in-the-string.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+	def firstAlphabet(self, S):
+		# code here
+		S=S.split()
+		s=""
+		for word in S:
+		    s+=word[0]
+		    
+		return s
+		    
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		S = input()
+		ob = Solution()
+		answer = ob.firstAlphabet(S)
+		
+		print(answer)
+
+
+# } Driver Code Ends
